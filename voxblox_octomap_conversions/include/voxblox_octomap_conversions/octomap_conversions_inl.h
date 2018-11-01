3e5293beab57b84e5042201e65bce149a696cd52
magnus <magnusgaertner@gmail.com>
2018-11-01 14:53:28 +0100

    optional swri enabled on octomap conversions

--- voxblox_octomap_conversions/include/voxblox_octomap_conversions/octomap_conversions_inl.h
+++ voxblox_octomap_conversions/include/voxblox_octomap_conversions/octomap_conversions_inl.h
@@ -4,7 +4,12 @@
 #include <voxblox_octomap_conversions/octomap_conversions.h>
 #include <octomap_msgs/conversions.h>
 #include <octomap/OcTree.h>
+#define SWRI_ENABLED
 
+
+#ifdef SWRI_ENABLED
+    #include <swri_profiler/profiler.h>
+#endif
 
 namespace voxblox {
 
@@ -12,8 +17,18 @@ namespace voxblox {
     void serializeLayerAsOctomapMsg(const Layer <VoxelType> &layer,
                                     octomap_msgs::Octomap *msg, double surface_distance_threshold_factor) {
         octomap::OcTree tree(layer.voxel_size());
-        serializeLayerAsOctomap(layer, &tree, surface_distance_threshold_factor);
-        octomap_msgs::fullMapToMsg(tree, *msg);
+        {
+            #ifdef SWRI_ENABLED
+                SWRI_PROFILE("serializeLayerAsOctomap");
+            #endif
+            serializeLayerAsOctomap(layer, &tree, surface_distance_threshold_factor);
+        }
+        {
+            #ifdef SWRI_ENABLED
+                SWRI_PROFILE("fullMapToMsg");
+            #endif
+            octomap_msgs::fullMapToMsg(tree, *msg);
+        }
     }
 
     template<typename VoxelType>
@@ -39,20 +54,55 @@ namespace voxblox {
 
             for (size_t linear_index = 0; linear_index < num_voxels_per_block;
                  ++linear_index) {
-                Point coord = block.computeCoordinatesFromLinearIndex(linear_index);
-                const VoxelType &voxel = block.getVoxelByLinearIndex(linear_index);
 
-                if (voxel.weight > kMinWeight &&
-                    std::abs(voxel.distance) < surface_distance) {
-                    octree->setNodeValue(coord.x(), coord.y(), coord.z(),octree->getClampingThresMaxLog());
-                } /* else if(voxel.){
+                {
+                    #ifdef SWRI_ENABLED
+                        SWRI_PROFILE("tsdf_get_voxel_coordinates_and_value_by_linear_index");
+                    #endif
+                    const auto& coord = block.computeCoordinatesFromLinearIndex(linear_index);
+                    const auto& voxel = block.getVoxelByLinearIndex(linear_index);
+                    if (voxel.weight > kMinWeight &&
+                        std::abs(voxel.distance) < surface_distance) {
+                        {
+                            #ifdef SWRI_ENABLED
+                                SWRI_PROFILE("insert_into_octree");
+                            #endif
+                            octree->setNodeValue(coord.x(), coord.y(), coord.z(), octree->getClampingThresMaxLog());
+                        }
+                    } /* else if(voxel.){
                     octree->setNodeValue(coord.x(), coord.y(), coord.z(),octree->getClampingThresMinLog());
                 }*/
+                }
+
 
             }
         }
-        octree->updateInnerOccupancy();
+        {
+            #ifdef SWRI_ENABLED
+                SWRI_PROFILE("updateInnerOccupancy");
+            #endif
+            octree->updateInnerOccupancy();
+        }
     }
+
+    template <typename T>
+    void SerializeToByteArray_(const T& msg, std::vector<uint8_t>& destination_buffer)
+    {
+        const uint32_t length = ros::serialization::serializationLength(msg);
+        destination_buffer.resize( length );
+        //copy into your own buffer
+        ros::serialization::OStream stream(destination_buffer.data(), length);
+        ros::serialization::serialize(stream, msg);
+    }
+/*
+    void wrapLayerMsgToOctomapMsg(const voxblox_msgs::Layer &layer, const octomap_msgs::Octomap* msg){
+
+    }
+    void unwrapLayerMsgFromOctomapMsg(voxblox_msgs::Layer* layer, octomap_msgs::Octomap& msg){
+
+    }*/
+
+
 
 }  // namespace voxblox
 
